41f93df4870618ab6b752c05a4f1254ecb06c201
Prasad Chandrashekar <prasad.chandrashekar@reachlocal.com>
2016-05-31 12:48:22 +0530

    Adding live comparision tests.

--- src/app/live-comparision/live-comparision.spec.ts
+++ src/app/live-comparision/live-comparision.spec.ts
@@ -9,6 +9,17 @@ import {
 import { LiveComparision } from './live-comparision.component';
 import { RepoManagerService } from './../services/repo-manager';
 
+let statsLeft = {
+  name: 'ABC',
+  owner: {
+    login: 'prasadc'
+  },
+  html_url: 'https://abc.com',
+  stargazers_count: 20,
+  forks_count: 20,
+  watchers_count: 20
+}, statsRight = statsLeft;
+
 describe('LiveComparision', () => {
   // provide our implementations or mocks to the dependency injector
   beforeEachProviders(() => [
@@ -21,10 +32,34 @@ describe('LiveComparision', () => {
   }));
   
   it('isStatsEmpty', inject([LiveComparision], (lc) => {
-    expect(lc.isStatsEmpty({})).toEqual(true);
+    expect(lc.isStatsEmpty()).toEqual(true);
   }));
   
   it('isStatsEmpty', inject([LiveComparision], (lc) => {
     expect(lc.isStatsEmpty({ left: { forks: 10 }})).toEqual(false);
-  }));  
+  }));
+  
+  it('save button', inject([LiveComparision], (lc) => {
+    expect(lc.saveButtonDisabled).toEqual(false);
+    expect(lc.saveButtonMessage).toEqual('Save Comparision');
+  }));
+  
+  it('activate', inject([LiveComparision], (lc) => {
+    lc.saveButtonDisabled =  true;
+    lc.activateButton();
+    expect(lc.saveButtonDisabled).toEqual(false);
+    expect(lc.saveButtonMessage).toEqual('Save Comparision');
+  }));
+  
+  it('saveComparison', inject([LiveComparision], (lc) => {
+    spyOn(lc._repoManagerService, 'addGame').and.callFake(() => {
+      return { on: (value, callback) => {
+        callback();
+      }};
+    });
+    lc.saveComparison(statsLeft, statsRight);
+    expect(lc.saveButtonDisabled).toEqual(true);
+    expect(lc.saveButtonMessage).toEqual('Comparision saved.');
+  }));
+
 });
